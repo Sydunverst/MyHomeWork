af443b69b34d82e28664b1aecd3a4358639cb658
Sydunverst <78224959+Sydunverst@users.noreply.github.com>
2021-04-08 16:34:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-var name = "Alex";
+var val1 = 1;
+let val2 = "hi";
+const val3 = 123.123;
 
-name ="Qwerty"
-console.log(name)
+console.log(val1); //printing value
+console.log(typeof val1); // printing type of value
+
+console.log(val2);
+console.log(typeof val2);
+
+console.log(val3);
+console.log(typeof val3);
